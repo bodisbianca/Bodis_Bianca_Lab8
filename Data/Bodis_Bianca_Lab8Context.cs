73becaac130f8deffd84e4b4d4dea1e327b3129a
BIANCA-ANA-MARIA BODIȘ <bianca.bodis@stud.ubbcluj.ro>
2020-11-25 21:24:13 +0200

    Add project files.

---
+++ Data/Bodis_Bianca_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Bodis_Bianca_Lab8.Models;
+
+namespace Bodis_Bianca_Lab8.Data
+{
+    public class Bodis_Bianca_Lab8Context : DbContext
+    {
+        public Bodis_Bianca_Lab8Context (DbContextOptions<Bodis_Bianca_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bodis_Bianca_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Bodis_Bianca_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Bodis_Bianca_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Bodis_Bianca_Lab8.Data;
+using Bodis_Bianca_Lab8.Models;
+
+namespace Bodis_Bianca_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Bodis_Bianca_Lab8.Data.Bodis_Bianca_Lab8Context _context;
+
+        public IndexModel(Bodis_Bianca_Lab8.Data.Bodis_Bianca_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.Include(b=>b.Publisher).ToListAsync();
+
+        }
+    }
+}
